b2f863e7743225237db20cfb613aba6c0340b2b7
Josh Frechette <josh.f.design@gmail.com>
2020-03-15 23:40:36 -0400

    added back production build.

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//  if (process.env.NODE_ENV === "production") {
-//      app.use(express.static("client/build"));
-//  }
+ if (process.env.NODE_ENV === "production") {
+     app.use(express.static("client/build"));
+ }
 
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
